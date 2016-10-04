fcbd8884e291756f9e99020749d34c5441fa0769
Michael Metrocavich <mmetro@users.noreply.github.com>
2016-10-04 13:01:50 -0400

    Lab 5

---
+++ lab5/MathFunctions/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/michael/Dropbox/CSCI2961/lab5/MathFunctions
+# Build directory: /home/michael/Dropbox/CSCI2961/lab5/MathFunctions
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
